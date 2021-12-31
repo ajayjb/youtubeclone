f933c00ff3b4317f54b540d797e7bbd9178402ed
Ajay <ajayjb11@gmail.com>
2021-12-31 17:55:42 +0530

    Final commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,11 +6,9 @@ export default function Footer() {
     <div className="footer">
       <h3 className="heading">Youtube</h3>
       <p className="para">Made with ❤ by ajayjb</p>
-      <div>
-        <a className="foottwi" href="https://twitter.com/ajay_j_b">
-          <i className="footerTwitter fab fa-twitter fa-md"></i>
-        </a>
-      </div>
+      <a href="https://twitter.com/ajay_j_b">
+        <i className="footerTwitter fab fa-twitter fa-md"></i>
+      </a>
     </div>
   );
 }
